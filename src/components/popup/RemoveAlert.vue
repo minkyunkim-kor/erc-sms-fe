1a2844b4dfd99f0e0382612b39dae9eb9fbf8b07
minkyunkim-kor <mingyun2621@naver.com>
2021-05-22 12:59:03 +0900

    학생 삭제 경고창 문구 수정

--- src/components/popup/RemoveAlert.vue
+++ src/components/popup/RemoveAlert.vue
@@ -1,10 +1,17 @@
 <template>
-  <v-dialog v-model="showDialog" persistent max-width="300px">
+  <v-dialog v-model="showDialog" persistent max-width="340px">
     <v-card>
       <v-card-title>
         <v-icon id="alert">mdi-close-circle-outline</v-icon>
         <p id="title">정말로 삭제 하시겠습니까?</p>
       </v-card-title>
+      <v-card-subtitle v-if="component === 'student'">
+        <p id="subtitle">
+          해당 학생 정보를 삭제할 경우, 인적정보 뿐만 아니라 평가데이터, 학습
+          정도, 수납 및 미납 내역, 현금영수증 등의 모든 정보가 삭제됩니다. 한 번
+          삭제된 정보는 복구가 불가능합니다.
+        </p>
+      </v-card-subtitle>
       <v-card-actions>
         <v-spacer />
         <v-btn id="action-btn" text @click="clickCancel">취소</v-btn>
@@ -92,9 +99,22 @@ export default {
   padding-top: 10px !important;
   padding-bottom: 4px !important;
 }
+.v-card .v-card__subtitle {
+  background-color: #ee534f !important;
+  padding-left: 10px !important;
+  padding-right: 10px !important;
+  padding-top: 10px !important;
+  padding-bottom: 4px !important;
+}
 #title {
   font-family: "NanumSquareRound", Avenir, Helvetica, Arial, sans-serif !important;
   font-size: 14px !important;
+  color: white !important;
+  margin-bottom: 0px;
+}
+#subtitle {
+  font-family: "NanumSquareRound", Avenir, Helvetica, Arial, sans-serif !important;
+  font-size: 12px !important;
   color: white !important;
   margin-bottom: 0px;
 }
